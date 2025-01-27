9b9faa4ed39293669e8225298e161514c476d891
Yeabsira Tarekegn <yabahane@gmail.com>
2025-01-27 09:11:58 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">test</h1>
+      <Button>TEST</Button>
+    </div>
+  );
+};
+
+export default App;
